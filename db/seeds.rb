0b0ddb923999f7e478d990a23e3a82ffe65ed566
Marco Silva <marcowilliamdf@gmail.com>
2015-12-06 17:05:43 -0200

    adding delete button for the owner of a discipline

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-User.create(id:1, name:"Funcionário", email:"funcionario@gmail.com", password:"123456", is_admin:false)
-User.create(id:2, name:"Administrador", email:"administrador@gmail.com", password:"1234567", is_admin:true)
+#User.create(id:1, name:"Funcionário", email:"funcionario@gmail.com", password:"123456", is_admin:false)
+User.create(id:1, name:"Administrador", email:"administrador@gmail.com", password:"1234567", is_admin:true)
 #Feedback.create(id:3, subject:"test", author:"alo", ActivityProcess_id:1)
